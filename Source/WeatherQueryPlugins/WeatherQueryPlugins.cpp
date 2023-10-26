3b4810085d2a8cde4b9bf3e77a8628256eedff0f
Wallace <135297518+OTTOWallace@users.noreply.github.com>
2023-10-26 17:57:08 +0800

    Delete Source directory

--- Source/WeatherQueryPlugins/WeatherQueryPlugins.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WeatherQueryPlugins.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WeatherQueryPlugins, "WeatherQueryPlugins" );
